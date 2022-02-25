55f9df71fb397233071d9193330d68f671784b39
Alexsandr Litkevich <alexshow2362@gmail.com>
2022-02-25 12:27:25 +0400

    first

---
+++ server/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+//Run program
+func main() {
+	srv := new(app.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("Error port", err.Error())
+	}
+
+}
+
+/*
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello Alex")
+	})
+	http.ListenAndServe(":8081", nil)
+
+*/
