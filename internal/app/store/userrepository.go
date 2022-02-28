597990a6cd9582b3e44c8422c72cc0722e6b396f
Alexsandr Litkevich <alexshow2362@gmail.com>
2022-02-28 13:06:24 +0400

    Added user

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -11,12 +11,12 @@ func (r *UserRepository) Create(u *model.User) (*model.User, error) {
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		u.Email,
 		u.EncryptedPassword,
-		).Scan(&u.ID); err != nil {
-		return err
+	).Scan(&u.ID); err != nil {
+		return nil, err
 	}
 	return u, nil
 }
 
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
-	return nil nil
+	return nil, nil
 }
